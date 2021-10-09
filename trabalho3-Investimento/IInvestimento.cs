d938d0f498891c7d1f510292e0c2a260f547a870
SrBurns <caiobl.25@gmail.com>
2021-10-09 15:05:21 -0300

    Adicionar arquivos de projeto.

---
+++ trabalho3-Investimento/IInvestimento.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trabalho3_Investimento
+{
+    interface IInvestimento
+    {
+    }
+}
---
+++ trabalho3-Investimento/Program.cs
@@ -0,0 +1,12 @@
+﻿  using System;
+
+namespace trabalho3_Investimento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
